ea099412bc396679a59f58f95cbfea6395e30dd1
DanielNduati <dannduati2@gmail.com>
2022-04-26 22:49:55 +0300

    Add the register_tortoise helper function from the util : tortoise.contrib.fastapi
    This function sets up Tortoise-ORM on startup and cleans up on teardown
    [Reference](https://tortoise-orm.readthedocs.io/en/latest/contrib/fastapi.html#tortoise.contrib.fastapi.register_tortoise)

--- project/app/main.py
+++ project/app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, Depends
-from app.config import get_settings, Settings
+from app.config import get_settings, Settings,settingz
+from tortoise.contrib.fastapi import register_tortoise
+
 app = FastAPI()
 
+register_tortoise(
+    app=app,
+    db_url=settingz.database_url,
+    modules={"models":["app.models.tortoise"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
 
 @app.get("/ping")
 async def pong(settings:Settings = Depends(get_settings)):
