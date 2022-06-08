69af2ffd46386342998143837c408034329b9537
DanielNduati <dannduati2@gmail.com>
2022-06-08 23:23:19 +0300

    Create a generate schema function
    This function applies the schema to the database in its final state
    instead of using Aerich which can be slow.

--- project/app/db.py
+++ project/app/db.py
@@ -1,7 +1,11 @@
+import logging
 import os
 
 from fastapi import FastAPI
+from tortoise import Tortoise, run_async
 from tortoise.contrib.fastapi import register_tortoise
+
+log = logging.getLogger("uvicorn")
 
 TORTOISE_ORM = {
     "connections": {"default": os.environ.get("DATABASE_URL")},
@@ -22,3 +26,19 @@ def init_db(app: FastAPI):
         generate_schemas=False,
         add_exception_handlers=True,
     )
+
+
+async def generate_schema() -> None:
+    log.info("Initializing Tortoise...")
+
+    await Tortoise.init(
+        db_url=os.environ.get("DATABASE_URL"),
+        modules={"models": ["models.tortoise"]},
+    )
+    log.info("Generating database schema via Tortoise...")
+    await Tortoise.generate_schemas()
+    await Tortoise.close_connections()
+
+
+if __name__ == "__main__":
+    run_async(generate_schema())
